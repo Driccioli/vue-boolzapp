47558a1b75644ab7cacb94416522753bfb3bc874
Driccioli <driccioli53@gmail.com>
2021-07-02 16:42:12 +0200

    Completata milestone 5-1, ma è presente un bug se si prova a cancellare l'unico messaggio rimanente in chat

--- boolzapp.js
+++ boolzapp.js
@@ -15,22 +15,26 @@ var app= new Vue({
                 {
                     date: '10/01/2020 15:30:55',
                     text: 'Hai portato a spasso il cane?',
-                    status: 'sent'
+                    status: 'sent',
+                    dropdown:false,
                 },
                 {
                     date: '10/01/2020 15:50:00',
                     text: 'Ricordati di dargli da mangiare',
-                    status: 'sent'
+                    status: 'sent',
+                    dropdown:false,
                 },
                 {
                     date: '10/01/2020 16:15:22',
                     text: 'Tutto fatto!',
-                    status: 'recieved'
+                    status: 'recieved',
+                    dropdown:false,
                 },
                 {
                     date: '10/01/2020 16:18:45',
                     text: 'Fatto? Bene!',
-                    status: 'sent'
+                    status: 'sent',
+                    dropdown:false,
                 }
             ],
 
@@ -43,17 +47,20 @@ var app= new Vue({
                     {
                         date: '20/03/2020 16:30:00',
                         text: 'Ciao come stai?',
-                        status: 'sent'
+                        status: 'sent',
+                        dropdown:false,
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         text: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'recieved'
+                        status: 'recieved',
+                        dropdown:false,
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
+                        status: 'sent',
+                        dropdown:false,
                     }
                 ],
             },
@@ -66,17 +73,20 @@ var app= new Vue({
                     {
                         date: '28/03/2020 10:10:40',
                         text: 'La Marianna va in campagna',
-                        status: 'recieved'
+                        status: 'recieved',
+                        dropdown:false,
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         text: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
+                        status: 'sent',
+                        dropdown:false,
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         text: 'Ah scusa!',
-                        status: 'recieved'
+                        status: 'recieved',
+                        dropdown:false,
                     }
                 ],
 
@@ -89,12 +99,14 @@ var app= new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        status: 'sent',
+                        dropdown:false,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Si, ma preferirei andare al cinema',
-                        status: 'recieved'
+                        status: 'recieved',
+                        dropdown:false,
                     }
                 ],
             },
@@ -126,7 +138,8 @@ var app= new Vue({
             this.contacts[this.currentContact].messages.push({
                 date:dateTimeString,
                 text:this.userMessage,
-                status:"sent"
+                status:"sent",
+                dropdown:false,
             });
             this.userMessage="";
             setTimeout(function(){
@@ -146,6 +159,20 @@ var app= new Vue({
                     return nome.includes(search);     
             } else{
                 return true;
+            }
+        },
+        deleteMessage(messages, key){
+            if(key=== messages.length-1){
+                messages.pop(key);
+            }   else{
+            messages.splice(key,1);
+            }
+        },
+        openMenu(message){
+            if(!message.dropdown){
+                message.dropdown = true;
+            }   else{
+                message.dropdown = false;
             }
         }
     }
